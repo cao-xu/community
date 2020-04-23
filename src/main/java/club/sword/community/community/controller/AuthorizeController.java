0b08bdc7f3c1ff068a5d762f59e9a19b9b898a18
cao-xu <xucao1130@163.com>
2020-04-23 21:00:26 +0800

    持久化登录

--- src/main/java/club/sword/community/community/controller/AuthorizeController.java
+++ src/main/java/club/sword/community/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 
@@ -40,7 +42,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -64,7 +67,9 @@ public class AuthorizeController {
             //使用MyBatis实现插入用户信息
             userMapper.insert(user);//类似于UserDAO，不过更简洁、方便
             //登录成功，写cookie和session
-            request.getSession().setAttribute("user", githubUser);
+            //在cookie中设置一个token
+            Cookie cookie = new Cookie("token", token);
+            response.addCookie(cookie);
             //返回根目录，index.html
             return "redirect:/";
         }else{
--- src/main/java/club/sword/community/community/controller/indexController.java
+++ src/main/java/club/sword/community/community/controller/indexController.java
@@ -1,13 +1,33 @@
 package club.sword.community.community.controller;
 
+import club.sword.community.community.mapper.UserMapper;
+import club.sword.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class indexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        //获取cookie中的token
+        //按token查询user，若存在则将user存入session，否则直接重定向回index.html
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies){
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                request.getSession().setAttribute("user",user);
+                break;
+            }
+        }
         return "index";
     }
 }
