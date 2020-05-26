cd54ee53855b960656e6393e4b6a28e6511e259f
cao-xu <xucao1130@163.com>
2020-05-26 09:52:06 +0800

    服务器拷贝上传图片参数配置文件

--- src/main/java/club/sword/community/config/AliyunParams.java
+++ src/main/java/club/sword/community/config/AliyunParams.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Component;
 /**
  * Created by XuCao on 2020/5/18
  */
+/*
+* @component （把普通pojo实例化到spring容器中）
+* 泛指各种组件，就是说当我们的类不属于各种归类的时候
+* （不属于@Controller、@Services等的时候），我们就可以使用@Component来标注这个类。
+*
+*/
 @Component
 @ConfigurationProperties(prefix = "aliyun")
 @Data
